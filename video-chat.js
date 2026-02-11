ca0977d845fec1757c749dae7edfcbad52acd1cb
Aleksandr Kravchuk 🍕 <v-kravchukal@microsoft.com>
2026-02-11 14:32:37 +0100

    chat

--- video-chat.js
+++ video-chat.js
@@ -32,7 +32,7 @@ async function startChat(){
     await startCamera();
     initPeer();
 
-    ws = new WebSocket("wss://free.blr2.piesocket.com/v3/1?api_key=erThvvWxtJljLufJ79d2dGOrd4CQXtBpebRTjOuV&notify_self=1");
+    ws = new WebSocket("wss://free.blr2.piesocket.com/v3/1?api_key=erThvvWxtJljLufJ79d2dGOrd4CQXtBpebRTjOuV");
     
     ws.onopen = async ()=>{
         log("Подключено к сигналингу, ожидаем соединения...");
